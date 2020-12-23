3cc3fa28c80ea4163c969389c6d73597add6a400
Richard Piazza <richard@richardpiazza.com>
2020-12-23 07:52:44 -0600

    Preview Tests

--- Tests/LocaleSupportTests/LocalizerTests.swift
+++ Tests/LocaleSupportTests/LocalizerTests.swift
@@ -1,36 +1,15 @@
 import XCTest
 @testable import localizer
-import class Foundation.Bundle
 
-final class LocalizerTests: XCTestCase {
+final class LocalizerTests: LocalizerTestCase {
     
     static var allTests = [
         ("textExecute", testExecute),
     ]
     
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-        #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-        #else
-        return Bundle.main.bundleURL
-        #endif
-    }
-    
     func testExecute() throws {
-        let pipe = Pipe()
-        
-        let process = Process()
-        process.executableURL = productsDirectory.appendingPathComponent("localizer")
-        process.standardOutput = pipe
         try process.run()
         process.waitUntilExit()
-        
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
         
         XCTAssertEqual(output, """
         OVERVIEW: Android 'strings.xml' & Apple 'Localizable.strings' utility.
